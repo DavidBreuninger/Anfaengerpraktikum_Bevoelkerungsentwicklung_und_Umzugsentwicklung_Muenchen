17a37447fdc936f3b0106c8827107f40b438fc4a
Ferdinand Balser <feba2806@gmail.com>
2026-04-07 23:44:52 +0200

    prepared for submission

--- Geodatensatz München.R
+++ Geodatensatz München.R
@@ -9,7 +9,7 @@ library("stringr")
 
 #Datensätze einlesen
 #https://opendata.muenchen.de/dataset/vablock_stadtbezirke_opendata
-read.csv("Data/vablock_stadtbezirk.csv")
+vablock_stadtbezirk <- read.csv("Data/vablock_stadtbezirk.csv")
 Mobilitaet <- read.csv("Data/indikat2510_bevoelkerung_mobilitaetsziffer_28_10_25.csv")
 Bevoelkerungsdichte <- read.csv("Data/indikat2510_bevoelkerung_bevoelkerungsdichte_28_10_25.csv")
 read_csv("Data/indikat2510_bevoelkerung_mobilitaetsziffer_28_10_25.csv")
@@ -28,64 +28,10 @@ ggplot(bezirke) +
         axis.ticks = element_blank(),
         axis.title = element_blank())
 
-#Bezirksnamen kürzen
-bezirkkurz <- bezirke %>%
-  group_by(sb_nummer, sb_name) %>%
-  summarise(geometry = st_union(geometry), .groups = "drop") %>%
-  mutate(short_name = sb_name)
-
-bezirkkurz$short_name <- bezirkkurz$short_name %>%
-  str_replace_all("Ludwigsvorstadt-Isarvorstadt", "Ludwigsv.-Isarv.") %>%
-  str_replace_all("Schwanthalerhöhe", "Schwanthalerh.") %>%
-  str_replace_all("Au-Haidhausen", "Au-Haidh.") %>%
-  str_replace_all("Altstadt-Lehel", "Altst.-Lehel") %>%
-  str_replace_all("Thalkirchen-Obersendling-Forstenried-Fürstenried-Solln",
-                  "Thalk.-Obersendl.-Forstenr.-Fürstenr.-Solln")
-
-
-centroids <- st_centroid(bezirkkurz)
-centroids <- st_point_on_surface(bezirkkurz)
-ggplot(bezirkkurz) +
-  geom_sf(fill = "lightgrey", color = "black") +
-  geom_sf_text(data = centroids, aes(label = short_name), size = 3)
-
-#Bestimmte Stadtbezirke hervorheben
-highlight <- c(
-  "Altstadt-Lehel",
-  "Ludwigsvorstadt-Isarvorstadt",
-  "Sendling",
-  "Schwabing-Freimann",
-  "Trudering-Riem",
-  "Aubing-Lochhausen-Langwied"
-)
-
-bezirkkurz2 <- bezirkkurz %>%
-  mutate(highlight = sb_name %in% highlight)
-
-ggplot(bezirkkurz2) +
-  geom_sf(aes(fill = highlight), color = "black") +
-  scale_fill_manual(values = c("grey85", "green")) +
-  theme(axis.text = element_blank(),
-        axis.ticks = element_blank(),
-        axis.title = element_blank()) +
-  theme(legend.position = "none")
-
-ggplot(bezirkkurz2) +
-  geom_sf(aes(fill = highlight), color = "black") +
-  scale_fill_manual(values = c("grey", "orange")) +
-  geom_sf_text(
-    data = bezirkkurz2 %>% filter(highlight),
-    aes(label = short_name),
-    size = 3
-  ) +
- theme(legend.position = "none") +
-  theme(axis.text = element_blank(),
-        axis.ticks = element_blank(),
-        axis.title = element_blank())
 
 #Mobilitätsdatensatz für join vorbereiten
 Mobilität_clean <- Mobilitaet %>%
-  mutate(bezirk_num = sub(" .*", "", Raumbezug)) %>%
+  mutate(bezirk_num = as.numeric(sub(" .*", "", Raumbezug))) %>%
   mutate(Zuzügegesamt = Basiswert.1 + Basiswert.2) %>%
   mutate(Wegzügegesamt = Basiswert.3 + Basiswert.4)
 Mobilität_cleaner <- Mobilität_clean %>%
@@ -95,221 +41,127 @@ Mobilität_cleaner <- Mobilität_clean %>%
 plotdata <- bezirke %>%
   left_join(Mobilität_cleaner, by = c("sb_nummer" = "bezirk_num"))
 
-#plotdata für Zuzüge 2024 nicht deutsch vorbereiten und dann plotten
-plotdata2024Zuzügenichtdeutsch <- plotdata %>%
-  filter(Jahr == 2024, Ausprägung == "nichtdeutsch") %>%
-  mutate(Zuzügegesamt = Basiswert.1 + Basiswert.2)
 
-ggplot(plotdata2024Zuzügenichtdeutsch) + 
-  geom_sf(aes(fill = Zuzügegesamt)) +
-  scale_fill_viridis_c() +
-  theme_minimal() +
-  ggtitle("Zuzüge 2024 von nicht deutschen") +
-  theme(axis.text = element_blank(),
-        axis.ticks = element_blank(),
-        axis.title = element_blank())
+#plotdata für Zuzugs- und Wegzugsrate
+plotdatapi <- plotdata %>%
+  mutate(Zuzugsrate = (Zuzügegesamt / Basiswert.5) * 100,
+         Wegzugsrate = (Wegzügegesamt / Basiswert.5) * 100) %>%
+  filter(Ausprägung == "insgesamt")
 
+plotdatapd <- plotdata %>%
+  group_by(Raumbezug, Jahr) %>%
+  mutate(gesamt_bev = Basiswert.5[Ausprägung == "insgesamt"][1]) %>%
+  ungroup() %>%
+  filter(Ausprägung == "deutsch") %>%
+  mutate(Zuzugsrate = Zuzügegesamt / gesamt_bev * 100,
+         Wegzugsrate = Wegzügegesamt / gesamt_bev * 100)
 
-plotdata2024Zuzügeinsgesamt <- plotdata %>%
-  filter(Jahr == 2024, Ausprägung == "insgesamt")
+plotdatapnd <- plotdata %>%
+  group_by(Raumbezug, Jahr) %>%
+  mutate(gesamt_bev = Basiswert.5[Ausprägung == "insgesamt"][1]) %>%
+  ungroup() %>%
+  filter(Ausprägung == "nichtdeutsch") %>%
+  mutate(Zuzugsrate = Zuzügegesamt / gesamt_bev * 100,
+         Wegzugsrate = Wegzügegesamt / gesamt_bev * 100)
 
-ggplot(plotdata2024Zuzügeinsgesamt) + 
-  geom_sf(aes(fill = Zuzügegesamt)) +
-  scale_fill_viridis_c() +
-  theme_minimal() +
-  ggtitle("Zuzüge 2024 insgesamt")
-
-#plotdata für Wegzüge 2024 insgesamt
-plotdata2024Wegzügeinsgesamt <- plotdata %>% filter(Jahr == 2024, Ausprägung == "insgesamt")
-ggplot(plotdata2024Wegzügeinsgesamt) +
-  geom_sf(aes(fill = Wegzügegesamt)) +
-  scale_fill_viridis_c() +
-  theme_minimal() +
-  ggtitle("Wegzüge 2024 insgesamt")
-
-#plotdata für Wegzüge 2000 insgesamt
-plotdata2000Wegzügeinsgesamt <- plotdata %>% filter(Jahr == 2000, Ausprägung == "insgesamt")
-ggplot(plotdata2000Wegzügeinsgesamt) +
-  geom_sf(aes(fill = Wegzügegesamt)) +
-  scale_fill_viridis_c() +
-  theme_minimal() +
-  ggtitle("Wegzüge 2000 insgesamt")
-
-#Facet Wegzüge insgesamt
-plotdatafacet <- plotdata %>% filter(Ausprägung == "insgesamt")
-
-ggplot(plotdatafacet) +
-  geom_sf(aes(fill = Wegzügegesamt)) +
-  scale_fill_viridis_c() +
-  facet_wrap(~ Jahr) +
-  theme_minimal() +
-  labs(title = "Wegzüge insgesamt nach Stadtbezirk",
-       fill = "Wegzüge") +
-  theme(axis.text = element_blank(),
-              axis.ticks = element_blank(),
-              axis.title = element_blank())
-
-#Facet Wegzüge außerstädtisch
-ggplot(plotdatafacet) +
-  geom_sf(aes(fill = Basiswert.3)) +
-  scale_fill_viridis_c() +
-  facet_wrap(~ Jahr) +
-  theme_minimal() +
-  labs(title = "Wegzüge aus München nach Stadtbezirk",
-       fill = "Wegzüge innerstädtisch") +
-  theme(axis.text = element_blank(),
-        axis.ticks = element_blank(),
-        axis.title = element_blank())
-
-#Facet Wegzüge innerstädtisch
-ggplot(plotdatafacet) +
-  geom_sf(aes(fill = Basiswert.4)) +
-  scale_fill_viridis_c() +
-  facet_wrap(~ Jahr) +
-  theme_minimal() +
-  labs(title = "Wegzüge innerhalb Münchens nach Stadtbezirk",
-       fill = "Wegzüge innerstädtisch") +
-  theme(axis.text = element_blank(),
-        axis.ticks = element_blank(),
-        axis.title = element_blank())
-
-#Facet Zuzüge außerstädtisch
-ggplot(plotdatafacet) +
-  geom_sf(aes(fill = Basiswert.1)) +
-  scale_fill_viridis_c() +
-  facet_wrap(~ Jahr) +
-  theme_minimal() +
-  labs(title = "Zuzüge außerstädtisch nach Stadtbezirk",
-       fill = "Zuzüge außerstädtisch") +
-  theme(axis.text = element_blank(),
-        axis.ticks = element_blank(),
-        axis.title = element_blank())
-
-#Facet Zuzüge innerstädtisch
-ggplot(plotdatafacet) +
-  geom_sf(aes(fill = Basiswert.2)) +
-  scale_fill_viridis_c() +
-  facet_wrap(~ Jahr) +
-  theme_minimal() +
-  labs(title = "Zuzüge innerstädtisch nach Stadtbezirk",
-       fill = "Zuzüge innerstädtisch") +
-  theme(axis.text = element_blank(),
-        axis.ticks = element_blank(),
-        axis.title = element_blank())
-
-#Netto-Migration
-plotdataNetto <- plotdata %>%
-  filter(Ausprägung == "insgesamt") %>%
-  mutate(netto = (Basiswert.1 + Basiswert.2) - (Basiswert.3 + Basiswert.4))
-
-ggplot(plotdataNetto) +
-  geom_sf(aes(fill = netto)) +
-  scale_fill_gradient2(
-    low = "red",
-    mid = "white",
-    high = "blue",
-    midpoint = 0,
+#Zuzugsrate Nicht-Deutsche Staatsbürger
+ggplot(plotdatapnd) +
+  geom_sf(aes(fill = Zuzugsrate)) +
+  scale_fill_viridis_c(
+    limits = c(0, 30),
+    values = scales::rescale(c(0, 5, 10, 20, 30)),
+    breaks = c(0, 5, 10, 20, 30)
   ) +
   facet_wrap(~ Jahr) +
-  labs(title = "Netto Migration") +
   theme_minimal() +
-  theme(
-    axis.text = element_blank(),
-    axis.ticks = element_blank(),
-    axis.title = element_blank()
-  )
-
-#Wegzüge insgesamt alle fünf Jahre
-plotdatand <- plotdata %>%
-  filter(Ausprägung == "nichtdeutsch")
-
-plotdatad <- plotdata %>%
-  filter(Ausprägung == "deutsch")
-
-ggplot(plotdatafacet) +
-  geom_sf(aes(fill = Wegzügegesamt)) +
-  scale_fill_viridis_c() +
-  facet_wrap(~ Jahr) +
-  theme_minimal() +
-  labs(title = "Wegzüge von 2000 bis 2024",
-       fill = "Anzahl Wegzüge") +
+  labs(title = "Zuzugsrate von Nicht-Deutschen",
+       fill = "Zuzugsrate") +
   theme(axis.text = element_blank(),
         axis.ticks = element_blank(),
         axis.title = element_blank())
 
-ggplot(plotdatafacet) +
-  geom_sf(aes(fill = Zuzügegesamt)) +
-  scale_fill_viridis_c() +
+#Wegzugsrate von Nicht-Deutschen Staatsbürgern
+ggplot(plotdatapnd) +
+  geom_sf(aes(fill = Wegzugsrate)) +
+  scale_fill_viridis_c(
+    limits = c(0, 35),
+    values = scales::rescale(c(0, 5, 10, 20, 30, 35)),
+    breaks = c(0, 10, 20, 30, 35)
+  ) +
   facet_wrap(~ Jahr) +
   theme_minimal() +
-  labs(title = "Zuzüge von 2000 bis 2024",
-       fill = "Anzahl Zuzüge") +
+  labs(title = "Wegzugsrate von Nicht-Deutschen",
+       fill = "Wegzugsrate") +
   theme(axis.text = element_blank(),
         axis.ticks = element_blank(),
         axis.title = element_blank())
 
-ggplot(plotdatand) +
-  geom_sf(aes(fill = Wegzügegesamt)) +
-  scale_fill_viridis_c() +
-  facet_wrap(~ Jahr) +
-  theme_minimal() +
-  labs(title = "Wegzüge von nicht-deutschen",
-       fill = "Anzahl Wegzüge") +
-  theme(axis.text = element_blank(),
-        axis.ticks = element_blank(),
-        axis.title = element_blank())
-
-ggplot(plotdatand) +
-  geom_sf(aes(fill = Zuzügegesamt)) +
-  scale_fill_viridis_c() +
-  facet_wrap(~ Jahr) +
-  theme_minimal() +
-  labs(title = "Zuzüge von nicht-deutschen",
-       fill = "Anzahl Zuzüge") +
-  theme(axis.text = element_blank(),
-        axis.ticks = element_blank(),
-        axis.title = element_blank())
-
-ggplot(plotdatad) +
-  geom_sf(aes(fill = Wegzügegesamt)) +
-  scale_fill_viridis_c() +
-  facet_wrap(~ Jahr) +
-  theme_minimal() +
-  labs(title = "Wegzüge von deutschen",
-       fill = "Anzahl Wegzüge") +
-  theme(axis.text = element_blank(),
-        axis.ticks = element_blank(),
-        axis.title = element_blank())
-
-ggplot(plotdatad) +
-  geom_sf(aes(fill = Zuzügegesamt)) +
-  scale_fill_viridis_c() +
-  facet_wrap(~ Jahr) +
-  theme_minimal() +
-  labs(title = "Zuzüge von deutschen",
-       fill = "Anzahl Zuzüge") +
-  theme(axis.text = element_blank(),
-        axis.ticks = element_blank(),
-        axis.title = element_blank())
-
-plotdatap <- plotdata %>%
-  filter(Ausprägung == "nichtdeutsch") %>%
-  mutate(Zuzugsrate = (Zuzügegesamt / Basiswert.5) * 100)
-
-ggplot(plotdatap) +
+#Zuzugsrate insgesamt
+ggplot(plotdatapi) +
   geom_sf(aes(fill = Zuzugsrate)) +
-  scale_fill_viridis_c() +
+  scale_fill_viridis_c(
+    limits = c(10, 40),
+    values = scales::rescale(c(10, 20, 30, 40)),
+    breaks = c(10, 15, 20, 25, 30, 35, 40)
+  ) +
   facet_wrap(~ Jahr) +
   theme_minimal() +
-  labs(title = "Zuzüge von deutschen",
-       fill = "Anzahl Zuzüge") +
+  labs(title = "Zuzugsrate insgesamt",
+       fill = "Zuzugsrate") +
   theme(axis.text = element_blank(),
         axis.ticks = element_blank(),
         axis.title = element_blank())
+
+#Wegzugsrate insgesamt
+ggplot(plotdatapi) +
+  geom_sf(aes(fill = Wegzugsrate)) +
+  scale_fill_viridis_c(
+    limits = c(10, 40),
+    values = scales::rescale(c(0, 5, 10, 20, 30, 40)),
+    breaks = c(10, 15, 20, 25, 30, 35, 40)
+  ) +
+  facet_wrap(~ Jahr) +
+  theme_minimal() +
+  labs(title = "Wegzugsrate insgesamt",
+       fill = "Wegzugsrate") +
+  theme(axis.text = element_blank(),
+        axis.ticks = element_blank(),
+        axis.title = element_blank())
+
+#Zuzugsrate von Deutschen Staatsbürgern
+ggplot(plotdatapd) +
+  geom_sf(aes(fill = Zuzugsrate)) +
+  scale_fill_viridis_c(
+    limits = c(0, 15),
+    breaks = c(0, 5, 10, 15)
+  ) +
+  facet_wrap(~ Jahr) +
+  theme_minimal() +
+  labs(title = "Zuzugsrate von Deutschen",
+       fill = "Zuzugsrate") +
+  theme(axis.text = element_blank(),
+        axis.ticks = element_blank(),
+        axis.title = element_blank())
+
+#Wegzugsrate von Deutschen Staatsbürgern
+ggplot(plotdatapd) +
+  geom_sf(aes(fill = Wegzugsrate)) +
+  scale_fill_viridis_c(
+    limits = c(0, 15),
+    breaks = c(0, 5, 10, 15)
+  ) +
+  facet_wrap(~ Jahr) +
+  theme_minimal() +
+  labs(title = "Wegzugsrate von Deutschen",
+       fill = "Wegzugsrate") +
+  theme(axis.text = element_blank(),
+        axis.ticks = element_blank(),
+        axis.title = element_blank())
+
 #Plots mit Bevölkerungsdatensatz
 #Bevölkerungsdatensatz für join vorbereiten
 Bevölkerung_clean <- indikat2510_bevoelkerung_bevoelkerungsdichte_28_10_25 %>%
-  mutate(bezirk_num = sub(" .*", "", Raumbezug)) %>%
+  mutate(bezirk_num = as.numeric(sub(" .*", "", Raumbezug))) %>%
   filter(grepl("^[0-9]{2} ", Raumbezug))
 
 #Join durchführen
@@ -325,11 +177,11 @@ minb <- min(abs(plotdataBevölkerungsentwicklung$Basiswert.1), na.rm = TRUE)
 ggplot(plotdataBevölkerungsentwicklung) +
   geom_sf(aes(fill = Basiswert.1)) +
   scale_fill_viridis_c(
-    limits = c(minb, maxb),
-    breaks = c(minb, 40000, 60000, 80000, 100000, maxb)
+    limits = c(20000, 125000),
+    breaks = c(20000, 40000, 60000, 80000, 100000, 125000)
   ) +
   theme_minimal() +
-  labs(title = "Bevölkerung 2024",
+  labs(title = "Bevölkerung",
        fill = "Anzahl Einwohner") +
   theme(axis.text = element_blank(),
         axis.ticks = element_blank(),
@@ -347,36 +199,20 @@ mind <- min(abs(plotdataBevölkerungsdichte$Bevölkerungsdichte), na.rm = TRUE) 
 ggplot(plotdataBevölkerungsdichte) +
   geom_sf(aes(fill = Bevölkerungsdichte)) +
   scale_fill_viridis_c(
-    limits = c(1780, 15600),
-    breaks = c(1780, 4000, 8000, 12000, 15600)
+    limits = c(0, 16000),
+    breaks = c(0, 4000, 8000, 12000, 16000)
   ) +
   theme_minimal() +
-  labs(title = "Bevölkerungsdichte nach Stadtbezirk 2024",
+  labs(title = "Bevölkerungsdichte",
        fill = "Dichte pro km²") +
   theme(axis.text = element_blank(),
         axis.ticks = element_blank(),
         axis.title = element_blank())
 
-#Alle drei Datensätze joinen
-Mobilität_suited <- Mobilität_cleaner %>%
-  filter(Ausprägung == "insgesamt") %>%
-  filter(Jahr >= 2002)
-
-mundb <- left_join(Bevölkerung_clean, Mobilität_suited, by = c("Raumbezug", "Jahr"))
-
-full <- bezirke %>%
-  left_join(mundb, by = c("sb_nummer" = "bezirk_num.x")) 
-
-#Neue Spalten
-fullnewcolumns <- full %>%
-  mutate(zuzugsrate = Zuzügegesamt / Basiswert.1.x * 100,
-         wegzugsrate = Wegzügegesamt / Basiswert.1.x * 100,
-         netto_rate = (Zuzügegesamt - Wegzügegesamt) / Basiswert.1.x * 100)
+#Netto-Migrationsraten
+#Netto-Migrationsrate insgesamt
 plotdataNettorate <- plotdataNetto %>%
   mutate(netto_rate = (Zuzügegesamt - Wegzügegesamt) / Basiswert.5 * 100)
-
-#Netto-Migrationsrate plotten
-max_abs <- max(abs(plotdataNettorated$netto_rate), na.rm = TRUE)
 
 ggplot(plotdataNettorate) +
   geom_sf(aes(fill = netto_rate)) +
@@ -398,8 +234,11 @@ ggplot(plotdataNettorate) +
 
 #Netto Migrationsrate für nicht-deutsche
 plotdataNettoratend <- plotdata %>%
+  group_by(Raumbezug, Jahr) %>%
+  mutate(gesamt_bev = Basiswert.5[Ausprägung == "insgesamt"][1]) %>%
+  ungroup() %>%
   filter(Ausprägung == "nichtdeutsch") %>%
-  mutate(netto_rate = (Zuzügegesamt - Wegzügegesamt) / Basiswert.5 * 100)
+  mutate(netto_rate = (Zuzügegesamt - Wegzügegesamt) / gesamt_bev * 100)
 
 ggplot(plotdataNettoratend) +
   geom_sf(aes(fill = netto_rate)) +
@@ -408,21 +247,24 @@ ggplot(plotdataNettoratend) +
     mid = "white",
     high = "blue",
     midpoint = 0,
-    limits = c(-19, 19),
-    breaks = c(-19, -15, -10, -5, 0, 5, 10, 15, 19)
+    limits = c(-6, 6),
+    breaks = c(-6, -3, 0, 3, 6)
   ) +
   facet_wrap(~ Jahr) +
   theme_minimal() +
-  labs(title = "Netto-Migrationsrate von nicht-deutschen",
-       fill = "Netto-Migrationsrate in Prozent") +
+  labs(title = "Netto-Migrationsrate von Nicht-Deutschen",
+       fill = "Rate") +
   theme(axis.text = element_blank(),
         axis.ticks = element_blank(),
         axis.title = element_blank())
 
 #Netto-Migrationsrate von deutschen
 plotdataNettorated <- plotdata %>%
+  group_by(Raumbezug, Jahr) %>%
+  mutate(gesamt_bev = Basiswert.5[Ausprägung == "insgesamt"][1]) %>%
+  ungroup() %>%
   filter(Ausprägung == "deutsch") %>%
-  mutate(netto_rate = (Zuzügegesamt - Wegzügegesamt) / Basiswert.5 * 100)
+  mutate(netto_rate = (Zuzügegesamt - Wegzügegesamt) / gesamt_bev * 100)
 
 ggplot(plotdataNettorated) +
   geom_sf(aes(fill = netto_rate)) +
@@ -436,8 +278,8 @@ ggplot(plotdataNettorated) +
   ) +
   facet_wrap(~ Jahr) +
   theme_minimal() +
-  labs(title = "Netto-Migrationsrate von deutschen",
-       fill = "Netto-Migrationsrate in Prozent") +
+  labs(title = "Netto-Migrationsrate von Deutschen",
+       fill = "Rate") +
   theme(axis.text = element_blank(),
         axis.ticks = element_blank(),
         axis.title = element_blank())
