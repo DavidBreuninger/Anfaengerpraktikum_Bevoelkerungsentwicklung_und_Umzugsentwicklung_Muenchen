5486c6b5f57290614e997240debcbd102bfdca6e
Wenyu Lou <wenyu.lou666@gmail.com>
2026-04-09 12:15:13 +0200

    change the color

--- plotpres.R
+++ plotpres.R
@@ -103,11 +103,14 @@ p2<-ggplot(mplot1, aes(x = Jahr, y = Basiswert.5, color = Ausprägung)) + geom_p
   geom_line() +labs(y = "mittlere Wohnbevölkerung in Millionen", 
                     title = "Einwohnerzahl nach Staatsbürgerschaft",
                     color="Staatsbürgerschaft")+
+  scale_colour_manual(values = c(deutsch = "#E69F00",
+                                 nichtdeutsch = "#0072B2",
+                                 insgesamt = "black"))+
   scale_y_continuous(labels = label_number(scale = 1e-6)) + theme_bw()+
   theme(plot.title = element_text(hjust = 0.5))
 p2
 
-ggsave("Results/p2.jpg", plot = p2,width = 10, height = 6)
+ggsave("Results/p_mittlere_Wohnbevölkerung.jpg", plot = p2,width = 10, height = 6)
 
 
 #Prozentuale Bevölkerungsentwicklung nach Stadtbezirke
@@ -125,10 +128,9 @@ bnew$Raumbezug <- factor(bnew$Raumbezug , levels = orderb$Raumbezug )
 
 p3 <-bnew %>% filter(Raumbezug != "Stadt München") %>% 
   ggplot(aes(x = Jahr, y = indexb1)) +
-  geom_point(color = "blue",size=1) +
-  geom_line(color = "blue")+ facet_wrap(~ Raumbezug) + 
-  labs(y = "Index (2002=100)",
-       title = "Potezentuale Bevölkerungsentwicklung nach Stadbezirken")+
+  geom_point(color = "#0072B2",size=1) +
+  geom_line(color = "#0072B2")+ facet_wrap(~ Raumbezug) + 
+  labs(y = "Bevölkerungsindex (2002=100)")+
   theme_bw()+
   theme(plot.title = element_text(hjust = 0.5),
         axis.text.x = element_text(angle = 45, hjust = 1),
@@ -136,7 +138,7 @@ p3 <-bnew %>% filter(Raumbezug != "Stadt München") %>%
         panel.spacing.x = unit(1.2, "lines"))
 p3
 
-ggsave("Results/p3.jpg", plot = p3,width = 10, height = 6)
+ggsave("Results/p_Prozentuale_Bevölkerungsentwicklung.jpg", plot = p3,width = 10, height = 6)
 
 
 #Prozentuale Bevölkerungsentwicklung in der Stadt München
@@ -148,10 +150,10 @@ bnew$Raumbezug <- factor(bnew$Raumbezug , levels = orderbb$Raumbezug )
 
 p3b <-bnew %>% filter(Raumbezug == "Stadt München") %>% 
   ggplot(aes(x = Jahr, y = indexb1)) +
-  geom_point(color = "blue",size=1) +
-  geom_line(color = "blue")+  
-  labs(y = "Index (2002=100)",
-       title = "Potezentuale Bevölkerungsentwicklung in der Stadt München")+
+  geom_point(color = "#0072B2",size=1) +
+  geom_line(color = "#0072B2")+  
+  labs(y = "Bevölkerungsindex (2002=100)",
+       title = "Prozentuale Bevölkerungsentwicklung in der Stadt München")+
   theme_bw() + 
   theme(plot.title = element_text(hjust = 0.5),
         axis.text.x = element_text(angle = 45, hjust = 1),
@@ -160,7 +162,7 @@ p3b <-bnew %>% filter(Raumbezug == "Stadt München") %>%
 
 p3b
 
-ggsave("Results/p3b.jpg", plot = p3b,width = 10, height = 6)
+ggsave("Results/p_Prozentuale_Bevölkerungsentwicklung.jpg", plot = p3b,width = 10, height = 6)
 
 
 
@@ -220,13 +222,16 @@ p5 <- mnew%>%
   filter(sn == 26)%>%
  ggplot( aes(x = Jahr, y = ar, color = Ausprägung)) + geom_point()+ 
   geom_line() + labs(y = "Nettoumzug (Zuzug – Wegzug) in Tausend",
-                     title = "Entwicklung in der Stadt München",
-                     color = "Staatsbürgerschaft") + 
+                     title = "Nettoumzug",
+                     color = "Staatsbürgerschaft")+
+  scale_color_manual(values = c(deutsch = "#E69F00",
+                                nichtdeutsch = "#0072B2",
+                                insgesamt = "black"))+
   scale_y_continuous(labels = label_number(scale = 1e-3)) +theme_bw()+
   theme(plot.title = element_text(hjust = 0.5))
 
 p5
-ggsave("Results/p5.jpg", plot = p5,width = 6, height = 4)
+ggsave("Results/p_Nettoumzug.jpg", plot = p5,width = 6, height = 4)
 
 
 #Nettozuzug
@@ -270,15 +275,18 @@ p7<- mnew%>%
   ggplot( aes(x = Jahr, y = Basiswert.1 + Basiswert.2, color = Ausprägung)) + geom_point()+ 
   geom_line() + facet_wrap(~Raumbezug) +
   labs(y = "Zuzug in Tausend", 
-       title = "Entwicklung in der Stadt München",
+       title = "Zuzug",
        color = "Staatsbürgerschaft") +
+  scale_color_manual(values = c(deutsch = "#E69F00",
+                                nichtdeutsch = "#0072B2",
+                                insgesamt = "black"))+
   scale_y_continuous(labels = label_number(scale = 1e-3)) + theme_bw()+
   theme(plot.title = element_text(hjust = 0.5),
         strip.text = element_blank())
   
 p7
 
-ggsave("Results/p7.jpg", plot = p7,width = 5, height = 3)
+ggsave("Results/p_Zuzug.jpg", plot = p7,width = 5, height = 3)
 
 
 #Entwicklung in der Stadt München
@@ -287,14 +295,17 @@ p8<-mnew%>%
   ggplot( aes(x = Jahr, y = Basiswert.3 + Basiswert.4, color = Ausprägung)) + geom_point()+ 
   geom_line() + facet_wrap(~Raumbezug) +
   labs(y = "Wegzug in Tausend", 
-       title = "Entwicklung in der Stadt München" ,
+       title = "Wegzug" ,
        color = "Staatsbürgerschaft") +
+  scale_color_manual(values = c(deutsch = "#E69F00",
+                                nichtdeutsch = "#0072B2",
+                                insgesamt = "black"))+
   scale_y_continuous(labels = label_number(scale = 1e-3)) +
   theme_bw()+ theme(plot.title = element_text(hjust = 0.5),
                     strip.text = element_blank())
 p8
 
-ggsave("Results/p8.jpg", plot = p8,width = 5, height = 3)
+ggsave("Results/p_Wegzug.jpg", plot = p8,width = 5, height = 3)
 
 
 #Entwicklung in den Stadtbezirken
@@ -384,9 +395,10 @@ p12<-mnew%>%
 ggplot(aes(x = Jahr, y = rb5, color = Ausprägung)) +
   geom_point(size=0.6)+ geom_line() +
   facet_wrap(~Raumbezug) +
-  labs(y= "Anteil", 
-       title = "Entwicklung der Staatsbürgerschaft in den Bezirken",
+  labs(y= "Anteil an der Bevölkerung", 
        color = "Staatsbürgerschaft")+
+  scale_color_manual(values = c(deutsch = "#E69F00",
+                                nichtdeutsch = "#0072B2"))+
   scale_y_continuous(limits = c(0,1))+
   theme_bw()+
   theme(plot.title = element_text(hjust = 0.5),
@@ -396,14 +408,16 @@ ggplot(aes(x = Jahr, y = rb5, color = Ausprägung)) +
   
 p12
 
-ggsave("Results/p12.jpg", plot = p12,width = 10, height = 6)
+ggsave("Results/p_Anteil_Bevölkerung.jpg", plot = p12,width = 8, height = 6)
+
 pnew<-mnew%>%
   filter(Ausprägung != "insgesamt" , sn == 26)%>%
   ggplot(aes(x = Jahr, y = rb5, color = Ausprägung)) +
   geom_point(size=0.6)+ geom_line() +
-  labs(y= "Anteil", 
-       title = "Entwicklung der Staatsbürgerschaft in der Stadt München",
+  labs(y= "Anteil an der Bevölkerung", 
        color = "Staatsbürgerschaft")+
+  scale_color_manual(values = c(deutsch = "#E69F00",
+                                nichtdeutsch = "#0072B2"))+
   scale_y_continuous(limits = c(0,1))+
   theme_bw()+
   theme(plot.title = element_text(hjust = 0.5),
@@ -412,7 +426,7 @@ pnew<-mnew%>%
         panel.spacing.x = unit(1.2, "lines"))
 
 pnew
-
+ggsave("Results/p_Anteil.jpg", plot = pnew,width = 8, height = 6)
 
 mnew <- mnew%>% 
   group_by(Raumbezug, Ausprägung, Jahr)%>%
