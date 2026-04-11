11aecbf80516a0a37e4b57ad3d1d3deb67356e6d
Duc Anh Pham <ducanhpham555@outlook.de>
2026-04-11 15:32:34 +0200

    changed comments, increased text size and changed title for axis

--- Umzugsentwicklung.R
+++ Umzugsentwicklung.R
@@ -4,7 +4,7 @@ library(data.table)
 library(scales)
 source("functions.R")
 
-# read 
+# read data
 Mobilitaet_thin <- read.csv("Clean_Data/Mobilitaet_thin.csv")
 wegzug_stadt <- read.csv("Clean_Data/umzug_innen_außen.csv")
 wegzug_Bezirksgruppen <-  read.csv("Clean_Data/umzug_Bezirksgruppen.csv")
@@ -24,15 +24,17 @@ wegzug_Bezirksgruppen_long <- wegzug_Bezirksgruppen %>%
                names_to = "Wegzug",
                values_to = "Anzahl")
 
-# stacked percentage barplot 
+# percent stacked barplot for "Wegzüge in Stadtbezirken" 
+# plot for slide 
 plot_wegzug_Stadt <- ggplot(wegzug_stadt_long, aes(x = Jahr, y = Anzahl, fill = Wegzug)) + 
   geom_bar(position = "fill", stat = "identity",  color = "white") +
   facet_wrap(~ Anfangsbezirk) +
   theme_bw() + 
   labs(fill = "Wegzug", y = "Anteil von Wegzügen")  +
   scale_fill_manual(values = c("außerstaedtisch" = "#F0D852", "innerstaedtisch" = "#8491B4")) + 
-  theme(axis.text.x = element_text(size = 7))
+  theme(axis.text.x = element_text(size = 7), legend.text = element_text(size = 11), legend.title = element_text(size = 12))
 
+# plot for slide
 plot_wegzug_Bezirke <- ggplot(wegzug_Bezirksgruppen_long, 
                              aes(x = Jahr, y = Anzahl, 
                                  fill = factor(Wegzug, levels = c("außerstaedtisch", "nicht benachbarte Bezirke", "Nachbarbezirke", "selber Bezirk")))) + 
@@ -41,22 +43,23 @@ plot_wegzug_Bezirke <- ggplot(wegzug_Bezirksgruppen_long,
   theme_bw() + 
   labs(fill = "Wegzug", y = "Anteil von Wegzügen") +
   scale_fill_manual(values = c("außerstaedtisch" = "#F0D852", "nicht benachbarte Bezirke" = "#009292", "Nachbarbezirke" = "#DB6D00", "selber Bezirk" = "#B66DFF")) +
-  theme(axis.text.x = element_text(size = 7))
+  theme(axis.text.x = element_text(size = 7), legend.text = element_text(size = 11), legend.title = element_text(size = 12))
 
 # save plots
 ggsave("Results/plot_wegzug_Stadt.jpg", plot = plot_wegzug_Stadt, width = 12, height = 8)
 ggsave("Results/plot_wegzug_Bezirke.jpg", plot = plot_wegzug_Bezirke, width = 12, height = 8)
 
-# Entwicklung Umzüge Münchens inner-/außerstaedtisch
-# pivot_to_long for plotting
+# Entwicklung Wegzüge Münchens inner-/außerstaedtisch
 
+# pivot to long for plotting
 Mobilitaet_long <- Mobilitaet_thin %>%
   pivot_longer(cols = c("innerstädtisch.Weggezogene..", "außerstädtisch.Weggezogene."), 
                names_to = "Wegzug",
                values_to = "Anzahl_Wegzug") %>%
   rename("mittlere_Bevölkerung" = mittlere.Hauptwohnsitzbevölkerung.)
-  
-# plot for Zuzug
+
+# mutate column with percentage of people moving away from a district in Munich, 
+# with column "Wegzug" differentiating between "inner-/außerstaedtisch"
 Mobilitaet_muenchen_weg <- Mobilitaet_long %>%
   filter(Ausprägung == "insgesamt" & Raumbezug == "Stadt München") %>%
   select(all_of(c("Jahr", "Wegzug", "Anzahl_Wegzug", "mittlere_Bevölkerung"))) %>%
@@ -65,28 +68,30 @@ Mobilitaet_muenchen_weg <- Mobilitaet_long %>%
     Wegzug == "außerstädtisch.Weggezogene." ~ "außerstaedtisch")) %>% 
   mutate(Prozent = Anzahl_Wegzug / mittlere_Bevölkerung * 100)
 
-# lineplot for Wegzug Muenchen
+# line plot for moving away from district in Munich, split in "inner-/außerstaedtisch"
 plot_stadt_prozent <- ggplot(Mobilitaet_muenchen_weg,
        aes(x = Jahr, y = Prozent, color = Wegzug)) +
   geom_point() + geom_line() +
-  labs(y = "Anteil in %", title = "Anteil Umzüge an mittlerer Bevölkerungzahl", color = "Wegzug") + 
+  labs(y = "Anteil in Prozent (Anzahl Wegzüge / mittl. Bevölkerung * 100)", color = "Wegzug") + 
   theme_bw() +
   scale_color_manual(values = c("#F0D852", "#8491B4")) +
-  theme(legend.text = element_text(size = 10))
+  theme(legend.text = element_text(size = 11), legend.title = element_text(size = 12))
+plot_stadt_prozent
 
-
+# mutate column with percentage of people moving away from a district in Munich, 
+# "inner-/außerstaedtisch" summed up for each year
 Mobilitaet_allg <- Mobilitaet_muenchen_weg %>%
   group_by(Jahr, mittlere_Bevölkerung) %>%
   summarise(Gesamtwegzug = sum(Anzahl_Wegzug)) %>%
   ungroup() %>%
   mutate(Prozent = Gesamtwegzug / mittlere_Bevölkerung * 100)
 
+# line plot with percentage of people moving away from a district in Munich
 plot_muenchen_prozent <- ggplot(Mobilitaet_allg, aes(x = Jahr,  y = Prozent)) +
   geom_point(color = "black") + geom_line(color = "black") +
-  labs(y = "Anteil in %", title = "Anteil Umzüge an mittlerer Bevölkerungszahl") +
+  labs(y = "Anteil in Prozent (Anzahl Wegzüge / mittl. Bevölkerung * 100)",) +
   theme_bw()
 
 # save plots
-
 ggsave("Results/plot_muenchen_prozent.jpg", plot = plot_muenchen_prozent, width = 12, height = 8)
 ggsave("Results/plot_stadt_prozent.jpg", plot = plot_stadt_prozent, width = 12, height = 8)
