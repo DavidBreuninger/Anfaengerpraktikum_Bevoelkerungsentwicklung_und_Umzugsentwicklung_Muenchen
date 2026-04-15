0bf3db546e005f89fa53eaa07db907c066b08df6
Duc Anh Pham <ducanhpham555@outlook.de>
2026-04-15 02:38:58 +0200

    setup file that installs and loads packages

---
+++ setup.R
@@ -0,0 +1,15 @@
+## This file will install and load all the necessary packages
+
+# vector of needed packages
+packages <- c("ggplot2", "readr", "sf", "tidyverse", "scales", "dplyr", "tidyr")
+
+# install packages
+if (length(setdiff(packages, rownames(installed.packages()))) != 0 ) {
+  install.packages(setdiff(packages, rownames(installed.packages())))
+} else {
+  print("Necessary packages already installed.")
+}
+
+
+# load packages
+lapply(packages, library, character.only = TRUE)
