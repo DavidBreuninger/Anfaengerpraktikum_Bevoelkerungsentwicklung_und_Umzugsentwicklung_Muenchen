b3a3b33138047ccdd8b0a7674f6fd2b9097f389d
Wenyu Lou <wenyu.lou666@gmail.com>
2026-04-15 17:33:59 +0200

    replace title

--- lineplot.R
+++ lineplot.R
@@ -92,7 +92,6 @@ plot1 <- mnew%>%
 
 p1<-ggplot(plot1, aes(x = Jahr, y = Basiswert.5, color = Ausprägung)) + geom_point() + 
   geom_line() +labs(y = "mittlere Wohnbevölkerung in Millionen", 
-                    title = "Einwohnerzahl",
                     color="Staatsbürgerschaft")+
   scale_colour_manual(values = c(deutsch = "#E69F00",
                                  nichtdeutsch = "#0072B2",
@@ -124,8 +123,7 @@ p2 <- mnew_index %>% filter(Raumbezug != "Stadt München",Ausprägung == "insges
   ggplot(aes(x = Jahr, y = indexb1)) +
   geom_point(color = "black",size=1) +
   geom_line(color = "black")+ facet_wrap(~ Raumbezug) + 
-  labs(y = "Bevölkerungsindex (2000=100)",
-       title = "Prozentuale Bevölkerungsentwicklung nach Stadtbezirken")+theme_bw()+
+  labs(y = "Bevölkerungsindex (2000=100)")+theme_bw()+
   theme(plot.title = element_text(hjust = 0.5), #Center the title
         axis.text.x = element_text(angle = 45, hjust = 1), #Rotate x-axis by 45°
         strip.text = element_text(size = 7,face="bold"), #bolded facet title
@@ -146,8 +144,7 @@ p2b <-mnew_index2 %>% filter(Raumbezug == "Stadt München", Ausprägung == "insg
   ggplot(aes(x = Jahr, y = indexb1)) +
   geom_point(color = "black",size=1) +
   geom_line(color = "black")+  
-  labs(y = "Bevölkerungsindex (2000=100)",
-       title = "Prozentuale Bevölkerungsentwicklung in der Stadt München")+theme_bw() +
+  labs(y = "Bevölkerungsindex (2000=100)")+theme_bw() +
   theme(plot.title = element_text(hjust = 0.5), #Center the title
         axis.text.x = element_text(angle = 45, hjust = 1)) #Rotate x-axis by 45°
 
@@ -170,7 +167,6 @@ p3<-mnew%>%
   geom_point(size=0.6)+ geom_line() +
   facet_wrap(~Raumbezug) +
   labs(y= "Anteil an der Bevölkerung",
-       title = "Entwicklung der Staatsbürgerschaft in den Bezirken",
        color = "Staatsbürgerschaft")+
   scale_color_manual(values = c(deutsch = "#E69F00",
                                 nichtdeutsch = "#0072B2"))+
@@ -192,7 +188,6 @@ p3b<-mnew%>%
   ggplot(aes(x = Jahr, y = rb5, color = Ausprägung)) +
   geom_point(size=0.6)+ geom_line() +
   labs(y= "Anteil an der Bevölkerung",
-       title = "Entwicklung der Staatsbürgerschaft in der Stadt München",
        color = "Staatsbürgerschaft")+
   scale_color_manual(values = c(deutsch = "#E69F00",
                                 nichtdeutsch = "#0072B2"))+
@@ -206,7 +201,7 @@ p3b<-mnew%>%
 p3b
 
  #save plot
-ggsave("Results/p_Anteil_Bevölkerung_München.jpg", plot = p3b,width = 8, height = 6)
+ggsave("Results/p_Anteil_Bevölkerung_München.jpg", plot = p3b,width = 10, height = 6)
 
 
 #This plot shows us the trend of net population inflow into München from 2000 to 2024.
