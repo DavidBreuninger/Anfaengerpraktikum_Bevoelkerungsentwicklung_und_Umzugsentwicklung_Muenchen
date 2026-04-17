0ae9f164f630607d494c45b47288e102988224eb
Ferdinand Balser <feba2806@gmail.com>
2026-04-17 22:00:54 +0200

    Wegzugsrate and Zuzugsrate

---
+++ Programs/Wegsrate_nach_Staatsbürgerschaft.R
@@ -0,0 +1,70 @@
+#mobility data adjusted
+Mobilität_clean <- Mobilitaet %>%
+  mutate(bezirk_num = as.numeric(sub(" .*", "", Raumbezug))) %>%
+  mutate(Zuzügegesamt = Basiswert.1 + Basiswert.2) %>%
+  mutate(Wegzügegesamt = Basiswert.3 + Basiswert.4)%>%
+  filter(grepl("^[0-9]{2} ", Raumbezug))
+
+#data adjusting
+Mobilität_sn <- Mobilität_clean %>%
+  mutate(
+    sn = case_when(
+      Raumbezug == "Stadt München" ~ 26,
+      TRUE ~ as.numeric(str_extract(Raumbezug, "^\\d+"))))
+plotdatal <- Mobilität_sn %>%
+  mutate(Raumbezug = case_when(
+    sn == 1 ~ "Altstadt",
+    sn == 2 ~ "Ludwigsvorstadt",
+    sn == 3 ~ "Maxvorstadt",
+    sn == 4 ~ "Schwabing-West",
+    sn == 5 ~ "Haidhausen",
+    sn == 6 ~ "Sendling",
+    sn == 7 ~ "Sendling-Westpark",
+    sn == 8 ~ "Schwanthalerhöhe",
+    sn == 9 ~ "Neuhausen",
+    sn == 10 ~ "Moosach",
+    sn == 11 ~ "Milbertshofen",
+    sn == 12 ~ "Schwabing",
+    sn == 13 ~ "Bogenhausen",
+    sn == 14 ~ "Berg am Laim",
+    sn == 15 ~ "Trudering",
+    sn == 16 ~ "Ramersdorf",
+    sn == 17 ~ "Obergiesing",
+    sn == 18 ~ "Untergiesing",                          
+    sn == 19 ~ "Thalkirchen",
+    sn == 20 ~ "Hadern",
+    sn == 21 ~ "Pasing",
+    sn == 22 ~ "Aubing",
+    sn == 23 ~ "Allach",
+    sn == 24 ~ "Feldmoching",
+    sn == 25 ~ "Laim",
+    TRUE ~ Raumbezug))
+
+plotdatals <- plotdatal %>%
+  group_by(Raumbezug, Jahr) %>%
+  mutate(gesamt_bev = Basiswert.5[Ausprägung == "insgesamt"][1]) %>%
+  ungroup() %>%
+  filter(Ausprägung %in% c("deutsch", "nichtdeutsch")) %>%
+  mutate(Zuzugsrate = (Zuzügegesamt / gesamt_bev) * 100,
+         Wegzugsrate = (Wegzügegesamt / gesamt_bev) * 100)
+
+#lineplot wegzugsrate by citizenship
+g15 <- ggplot(plotdatals, aes(x = Jahr, y = Wegzugsrate, color = Ausprägung)) +
+  geom_line() +
+  geom_point() +
+  facet_wrap(~ Raumbezug) +
+  theme_minimal()+
+  theme(axis.text.x = element_text(angle = 45, hjust = 1),
+        strip.text = element_text(size = 10),
+        panel.spacing.x = unit(1.2, "lines"),
+        panel.border = element_rect(color = "grey", fill = NA, linewidth = 0.5),
+        plot.title = element_text(size = 18, hjust = 0.5),
+        strip.background = element_rect(color = "grey", fill = "grey90")) +
+  scale_color_manual(values = c(deutsch = "#E69F00",
+                                nichtdeutsch = "#0072B2")) +
+  labs(y = "Wegzugsrate",
+       color = "Ausprägung")
+
+g15
+
+ggsave("Results/g15.jpg", plot = g15,width = 3, height = 3)
---
+++ Programs/Wegzugsrate_insgesamt.R
@@ -0,0 +1,65 @@
+#mobility data adjusted
+Mobilität_clean <- Mobilitaet %>%
+  mutate(bezirk_num = as.numeric(sub(" .*", "", Raumbezug))) %>%
+  mutate(Zuzügegesamt = Basiswert.1 + Basiswert.2) %>%
+  mutate(Wegzügegesamt = Basiswert.3 + Basiswert.4)%>%
+  filter(grepl("^[0-9]{2} ", Raumbezug))
+
+#data adjusting
+Mobilität_sn <- Mobilität_clean %>%
+  mutate(
+    sn = case_when(
+      Raumbezug == "Stadt München" ~ 26,
+      TRUE ~ as.numeric(str_extract(Raumbezug, "^\\d+"))))
+
+plotdatal <- Mobilität_sn %>%
+  mutate(Raumbezug = case_when(
+    sn == 1 ~ "Altstadt",
+    sn == 2 ~ "Ludwigsvorstadt",
+    sn == 3 ~ "Maxvorstadt",
+    sn == 4 ~ "Schwabing-West",
+    sn == 5 ~ "Haidhausen",
+    sn == 6 ~ "Sendling",
+    sn == 7 ~ "Sendling-Westpark",
+    sn == 8 ~ "Schwanthalerhöhe",
+    sn == 9 ~ "Neuhausen",
+    sn == 10 ~ "Moosach",
+    sn == 11 ~ "Milbertshofen",
+    sn == 12 ~ "Schwabing",
+    sn == 13 ~ "Bogenhausen",
+    sn == 14 ~ "Berg am Laim",
+    sn == 15 ~ "Trudering",
+    sn == 16 ~ "Ramersdorf",
+    sn == 17 ~ "Obergiesing",
+    sn == 18 ~ "Untergiesing",                          
+    sn == 19 ~ "Thalkirchen",
+    sn == 20 ~ "Hadern",
+    sn == 21 ~ "Pasing",
+    sn == 22 ~ "Aubing",
+    sn == 23 ~ "Allach",
+    sn == 24 ~ "Feldmoching",
+    sn == 25 ~ "Laim",
+    TRUE ~ Raumbezug))
+
+plotdatali <- plotdatal %>%
+  mutate(Zuzugsrate = (Zuzügegesamt / Basiswert.5) * 100,
+         Wegzugsrate = (Wegzügegesamt / Basiswert.5) * 100) %>%
+  filter(Ausprägung == "insgesamt")
+
+#lineplot Wegzugsrate total
+g12 <- ggplot(plotdatali, aes(x = Jahr, y = Wegzugsrate)) +
+  geom_line(color = "black", size = 1) +
+  geom_point() +
+  facet_wrap(~ Raumbezug) +
+  theme_minimal()+
+  theme(axis.text.x = element_text(angle = 45, hjust = 1),
+        strip.text = element_text(size = 10),
+        panel.spacing.x = unit(1.2, "lines"),
+        plot.title = element_text(size = 18, hjust = 0.5),
+        panel.border = element_rect(color = "grey", fill = NA, linewidth = 0.5),
+        strip.background = element_rect(color = "grey", fill = "grey90")) +
+  labs(y = "Wegzugsrate")
+
+g12
+
+ggsave("Results/g12.jpg", plot = g12,width = 3, height = 3)
