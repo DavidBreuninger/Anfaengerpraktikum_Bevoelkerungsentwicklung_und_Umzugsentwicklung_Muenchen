47c4dfb3c2f11ae097168218f27ef58ec3f6f729
Duc Anh Pham <ducanhpham555@outlook.de>
2026-04-18 20:57:03 +0200

    updated code using data set Mobilitaet_thin

--- Programs/Zuzugsrate_nach_Staatsbürgerschaft.R
+++ Programs/Zuzugsrate_nach_Staatsbürgerschaft.R
@@ -1,55 +1,19 @@
-#mobility data adjusted
-Mobilität_clean <- Mobilitaet %>%
-  mutate(bezirk_num = as.numeric(sub(" .*", "", Raumbezug))) %>%
-  mutate(Zuzügegesamt = Basiswert.1 + Basiswert.2) %>%
-  mutate(Wegzügegesamt = Basiswert.3 + Basiswert.4)%>%
-  filter(grepl("^[0-9]{2} ", Raumbezug))
+## this file contains the code for the line plot on slide 17
 
-#data adjusting
-Mobilität_sn <- Mobilität_clean %>%
-  mutate(
-    sn = case_when(
-      Raumbezug == "Stadt München" ~ 26,
-      TRUE ~ as.numeric(str_extract(Raumbezug, "^\\d+"))))
-plotdatal <- Mobilität_sn %>%
-  mutate(Raumbezug = case_when(
-    sn == 1 ~ "Altstadt",
-    sn == 2 ~ "Ludwigsvorstadt",
-    sn == 3 ~ "Maxvorstadt",
-    sn == 4 ~ "Schwabing-West",
-    sn == 5 ~ "Haidhausen",
-    sn == 6 ~ "Sendling",
-    sn == 7 ~ "Sendling-Westpark",
-    sn == 8 ~ "Schwanthalerhöhe",
-    sn == 9 ~ "Neuhausen",
-    sn == 10 ~ "Moosach",
-    sn == 11 ~ "Milbertshofen",
-    sn == 12 ~ "Schwabing",
-    sn == 13 ~ "Bogenhausen",
-    sn == 14 ~ "Berg am Laim",
-    sn == 15 ~ "Trudering",
-    sn == 16 ~ "Ramersdorf",
-    sn == 17 ~ "Obergiesing",
-    sn == 18 ~ "Untergiesing",                          
-    sn == 19 ~ "Thalkirchen",
-    sn == 20 ~ "Hadern",
-    sn == 21 ~ "Pasing",
-    sn == 22 ~ "Aubing",
-    sn == 23 ~ "Allach",
-    sn == 24 ~ "Feldmoching",
-    sn == 25 ~ "Laim",
-    TRUE ~ Raumbezug))
+#read data 
+Mobilitaet_thin <- read.csv("Clean_Data/Mobilitaet_thin.csv")
 
-plotdatals <- plotdatal %>%
+# adds column "Zuzugsrate", percentage of people moving to districg, split into "deutsch"/"nichtdeutsch"
+Zuzugsrate_split <- Mobilitaet_thin %>% 
   group_by(Raumbezug, Jahr) %>%
-  mutate(gesamt_bev = Basiswert.5[Ausprägung == "insgesamt"][1]) %>%
+  mutate(gesamt_bev = mittlere_Hauptwohnsitzbevölkerung[Ausprägung == "insgesamt"][1]) %>%
   ungroup() %>%
   filter(Ausprägung %in% c("deutsch", "nichtdeutsch")) %>%
-  mutate(Zuzugsrate = (Zuzügegesamt / gesamt_bev) * 100,
-         Wegzugsrate = (Wegzügegesamt / gesamt_bev) * 100)
+  mutate(Zuzugsrate = (Gesamtzuzug / gesamt_bev) * 100) %>%
+  filter(Raumbezug != "Stadt München")
 
 #lineplot Zuzugsrate by citizenship
-g14 <- ggplot(plotdatals, aes(x = Jahr, y = Zuzugsrate, color = Ausprägung)) +
+g14 <- ggplot(Zuzugsrate_split, aes(x = Jahr, y = Zuzugsrate, color = Ausprägung)) +
   geom_line() +
   geom_point(size = 0.9) +
   facet_wrap(~ Raumbezug) +
@@ -57,14 +21,14 @@ g14 <- ggplot(plotdatals, aes(x = Jahr, y = Zuzugsrate, color = Ausprägung)) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1),
         strip.text = element_text(size = 10),
         panel.spacing.x = unit(1.2, "lines"),
-        panel.border = element_rect(color = "grey", fill = NA, linewidth = 0.5),
-        plot.title = element_text(size = 18, hjust = 0.5),
-        strip.background = element_rect(color = "grey", fill = "grey90")) +
+        panel.border = element_rect(color = "grey", fill = NA, linewidth = 0.5), 
+        strip.background = element_rect(color = "grey", fill = "grey90"),
+        legend.text = element_text(size = 11)) +
   scale_color_manual(values = c(deutsch = "#E69F00",
                                 nichtdeutsch = "#0072B2")) +
   labs(y = "Zuzugsrate",
        color = "Ausprägung")
 
 g14
-
-ggsave("Results/g14.jpg", plot = g14,width = 3, height = 3)
+#save plot
+ggsave("Results/Zuzugsrate_Staatsbürgerschaft.jpg", plot = g14, width = 10, height = 8)
