41f514b1c7c3aec5a43247a06a9e757845f43e44
Duc Anh Pham <ducanhpham555@outlook.de>
2026-04-19 03:27:06 +0200

    changed code to use data set Mobilitaet_thin

--- Programs/Prozentuale_Bevölkerungsentwicklung.R
+++ Programs/Prozentuale_Bevölkerungsentwicklung.R
@@ -1,22 +1,24 @@
-##This file contains the code for the lineplot on slide 11.
+##This file contains the code for the line plot on slide 11.
 #Prepare for Data
 library("ggplot2")
 library("tidyverse")
 library("tidyr")
 library("dplyr")
 library("scales")
-mnew <- read.csv("Clean_Data/mnew.csv")
+
+Mobilitaet_thin <- read.csv("Clean_Data/Mobilitaet_thin.csv")
 
 #This plot shows us the population growth rate of each district compared to 2000.
 #Make the population into index (2000=100) and then sort the districts by the latest year.
 #Calculate the index : Current value / 2000 value × 100
-mnew_index <- mnew %>%
-  group_by(Raumbezug,Ausprägung) %>%
-  mutate(indexb1 = 100 * Basiswert.5 / Basiswert.5[Jahr == 2000])
+mnew_index <- Mobilitaet_thin %>%
+  group_by(Raumbezug, Ausprägung) %>%
+  mutate(indexb1 = 100 * mittlere_Hauptwohnsitzbevölkerung / mittlere_Hauptwohnsitzbevölkerung[Jahr == 2000]) %>%
+  ungroup()
 
 #Sort by latest year
 order1<- mnew_index%>% 
-  filter(Jahr == max(Jahr),Raumbezug != "Stadt München",Ausprägung == "insgesamt") %>% 
+  filter(Jahr == max(Jahr), Raumbezug != "Stadt München", Ausprägung == "insgesamt") %>% 
   arrange(desc(indexb1))
 
 #Redefining the order of zones,those with faster growth are listed first.
@@ -29,7 +31,7 @@ p2 <- mnew_index %>% filter(Raumbezug != "Stadt München",Ausprägung == "insges
   labs(y = "Bevölkerungsindex (2000=100)")+theme_bw()+
   theme(axis.text.x = element_text(angle = 45, hjust = 1), #Rotate x-axis by 45°
         strip.text = element_text(size = 7,face="bold"), #bolded facet title
-        panel.spacing.x = unit(1.2, "lines")) #Controlling the horizontal spacing of facet Grafik
+        panel.spacing.x = unit(1.2, "lines")) #Controlling the horizontal spacing of facets
 p2
 
 #save plot
--- Programs/Prozentuale_Bevölkerungsentwicklung_München.R
+++ Programs/Prozentuale_Bevölkerungsentwicklung_München.R
@@ -1,19 +1,21 @@
-##This file contains the code for the lineplot on slide 10.
+##This file contains the code for the line plot on slide 10.
 #Prepare for Data
 library("ggplot2")
 library("tidyverse")
 library("tidyr")
 library("dplyr")
 library("scales")
-mnew <- read.csv("Clean_Data/mnew.csv")
+
+Mobilitaet_thin <- read.csv("Clean_Data/Mobilitaet_thin.csv")
 
 #This plot shows us the population growth rate of München compared to 2000.
 #Calculate the index : Current value / 2000 value × 100
-mnew_index2 <- mnew %>%
-  group_by(Raumbezug,Ausprägung) %>%
-  mutate(indexb1 = 100 * Basiswert.5 / Basiswert.5[Jahr == 2000])
+mnew_index2 <- Mobilitaet_thin %>%
+  group_by(Raumbezug, Ausprägung) %>%
+  mutate(indexb1 = 100 * mittlere_Hauptwohnsitzbevölkerung / mittlere_Hauptwohnsitzbevölkerung[Jahr == 2000]) %>%
+  ungroup()
 
-p2b <-mnew_index2 %>% filter(Raumbezug == "Stadt München", Ausprägung == "insgesamt") %>% 
+p2b <- mnew_index2 %>% filter(Raumbezug == "Stadt München", Ausprägung == "insgesamt") %>% 
   ggplot(aes(x = Jahr, y = indexb1)) +
   geom_point(color = "black",size=1) +
   geom_line(color = "black")+  
